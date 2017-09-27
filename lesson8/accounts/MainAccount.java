cb213747675a957943e15f3f656f8ddf2db80137
balanenko10 <32300380+balanenko10@users.noreply.github.com>
2017-09-27 21:41:04 +0300

    Delete MainAccount.java

--- lesson8/accounts/MainAccount.java
+++
@@ -1,8 +0,0 @@
-package lesson8.accounts;
-
-public class MainAccount extends ChekingAccount {
-
-    public MainAccount(String bankName, String ownerName, int moneyAmount, int limitOfExpenses) {
-        super(bankName, ownerName, moneyAmount, limitOfExpenses);
-    }
-}
