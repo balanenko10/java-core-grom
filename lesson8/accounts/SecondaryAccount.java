2f8f86530828081b1fbda6633e6c123fc6b4ddfe
balanenko10 <32300380+balanenko10@users.noreply.github.com>
2017-09-27 21:40:26 +0300

    Delete SecondaryAccount.java

--- lesson8/accounts/SecondaryAccount.java
+++
@@ -1,8 +0,0 @@
-package lesson8.accounts;
-
-public class SecondaryAccount extends ChekingAccount {
-
-    public SecondaryAccount(String bankName, String ownerName, int moneyAmount, int limitOfExpenses) {
-        super(bankName, ownerName, moneyAmount, limitOfExpenses);
-    }
-}
